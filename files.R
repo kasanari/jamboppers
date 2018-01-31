2fe59ceb8af8c595adc871eeb07acc38f0d7401e
kasanari <jakobnnyberg@gmail.com>
2018-01-31 19:04:15 +0100

    Tried doing logistic regression

--- files.R
+++ files.R
@@ -1,4 +1,26 @@
-importFiles = function(path = ".") {
-  songs.train <- read.csv(file.path(path, "training_data.csv"), header=T)
-  songs.test <- read.csv(file.path(path, "songs_to_classify.csv"), header=T)
+# importFiles = function(path = ".") { 
+#   songs.train <- read.csv(file.path(path, "training_data.csv"), header=T)
+#   songs.test <- read.csv(file.path(path, "songs_to_classify.csv"), header=T)
+# }
+
+scaleData = function(data) {
+  names = names(data)
+  data.scaled = data
+  for (name in names) {
+    tryCatch({
+      #Catch the error that pops up when it tries to scale a non-numeric column
+      data.scaled[name] = scale(data[[name]])
+    },
+    error = function(e) {
+      cat(name, "is not a numeric column and can not be scaled\n")
+    })
+  }
+  return(data.scaled)
+}
+
+setQualitative = function(data, names){
+  for (name in names) {
+    data[name] = as.factor(data[[name]]) 
+  }
+  return(data)
 }
--- main.R
+++ main.R
@@ -1,22 +1,34 @@
+rm(list = ls()) #clears workspace
 
 source("files.R")
 
+set.seed(1)
+
 #importFiles()
 
-songs.train <- read.csv('training_data.csv', header=T)
-songs.test <- read.csv('songs_to_classify.csv', header=T)
 
 
-scaleData = function(data) {
-  names = names(data)
-  data.scaled = data
-  for (name in names) {
-    data.scaled[name] = scale(data[name])
-  }
-  return(data.scaled)
-}
+songs <- read.csv('training_data.csv', header = T)
 
+training_indices <- sample(nrow(songs), size = 250, replace = FALSE)
+
+qualitative_vars = list("label","key", "time_signature")
+songs = setQualitative(songs, qualitative_vars)
+
+songs.train = songs[training_indices,]
+songs.test = songs[-training_indices,]
 
 songs.train.scaled = scaleData(songs.train)
-#songs.scaledTrain$danceability = scale(songs.train$danceability)
-#songs.scaledTrain$energy = scale(songs.train$energy)
+
+f = label ~ key + danceability + energy + key + loudness + mode + speechiness + acousticness + instrumentalness + liveness + valence + time_signature    
+
+log_reg.fit = glm(formula = f, data = songs.train, family = "binomial")
+log_reg.probs = predict(object = log_reg.fit, newdata = songs.test, type="response")
+
+log_reg.pred <- rep("dislike", length(log_reg.probs))
+log_reg.pred[log_reg.probs > 0.5] <- "like"
+
+table(songs.test$label, log_reg.pred)
+print(mean(log_reg.pred == songs.test$label))
+
+#songs.classify <- read.csv('songs_to_classify.csv', header = T)
