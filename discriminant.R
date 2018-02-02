6641af27a9d92b9527c80172b5a1ffdfe7d6a7e2
nozhoven <a.andersdotter.gustavsson@gmail.com>
2018-02-02 15:37:35 +0100

    NU är det fint. Ta bort time_signature för att kunna köra qda

--- discriminant.R
+++ discriminant.R
@@ -1,4 +1,4 @@
-discriminant  = function(train, test, f) {
+linearda  = function(train, test, f) {
   #Uses function lda from library(MASS) to learn a 
   #linear discriminant analysis model using data "train" and "test"
   #which have labels contained in f.
@@ -8,6 +8,15 @@ discriminant  = function(train, test, f) {
   lda.testdata <- predict(object = lda.fit, newdata = test)
   #Places the "class" list from the prediction into a return variable
   lda.pred <- lda.testdata$class
-
   return(lda.pred)
+}  
+quadda  = function(train, test, f) {  
+  qda.fit <- qda(formula = f, data = train)
+  #Uses the model learned in lda.fit to predict class ("like" or "dislike")
+  #of testdata
+  qda.testdata <- predict(object = qda.fit, newdata = test)
+  #Places the "class" list from the prediction into a return variable
+  qda.pred <- qda.testdata$class
+  
+  return(qda.pred)
 }
