d29574575080acd4bdf0babdf534d71681bb15c2
Jakob Nyberg <jakobnnyberg@gmail.com>
2018-02-02 14:53:07 +0100

    i think i fixed it

--- makeWebsiteString.R
+++ makeWebsiteString.R
@@ -3,5 +3,7 @@ makeWebsiteString = function(sentList){
   websiteString <- rep(0, times = 1, length.out = length(sentList), each = 1)
   websiteString[sentList == "like"] <- 1
   
+  websitestring = paste(websiteString, "")
+  
   return(websiteString)
 }
